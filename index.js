7e864b059daf6a72bbaf57cc020759b9d0d21851
Harshil Findoliya <140339023+Harshilfi@users.noreply.github.com>
2024-03-21 23:07:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,53 @@ data=[
     {mail:"pqr@gmail.com",pass:"pqrst56"}
 
 ]
+
 function func(){
     event.preventDefault()
-    overlay=document.getElementById("blurreddiv")
-    modal1=document.getElementById("errorDialog")
-    modal2=document.getElementById("okDialog")
-    inputMail=document.getElementById("mail").value
-    inputPass=document.getElementById("pass").value
+    let overlay=document.getElementById("blurreddiv")
+    let modal1=document.getElementById("errorDialog")
+    let modal2=document.getElementById("okDialog")
+    let inputMail=document.getElementById("mail").value
+    let inputPass=document.getElementById("pass").value
     l=data.length
     i=0
+    console.log(inputPass)
     while(i<l){
-        if(inputMail==data[i].mail && inputPass==data[i].pass){
+        if(inputMail===data[i].mail && inputPass===data[i].pass){
+            console.log(inputPass)
             // alert("Logged in Succesfully")
-            overlay.classList.add("overlay");
-            modal2.open="true";
+            modal2.showModal();
             return ;
         }
         i+=1
     }
-    overlay.classList.add("overlay");
-    modal1.open="true";
+    modal1.showModal();
+}
+// function register(){
+//     overlay=document.getElementById("blurreddiv")
+//     modal1=document.getElementById("errorDialog")
+//     modal2=document.getElementById("okDialog")
+//     inputMail=document.getElementById("mail").value
+//     inputPass=document.getElementById("pass").value
+//     l=data.length
+//     i=0
+//     while(i<l){
+//         if(inputMail==data[i].mail){
+//             overlay.classList.add("overlay");
+//             modal1.open="true";
+//             return ;
+//         }
+//     }
+//     data.push({mail:"${inputMail}",pass:"${inputPass}"})
+//     overlay.classList.add("overlay");
+//     modal2.open="true";
+// }
+function closePop(){
+    let overlay=document.getElementById("blurreddiv")
+    let modal1=document.getElementById("errorDialog")
+    let modal2=document.getElementById("okDialog")
+
+    modal1.close();
+    modal2.close();
+    
 }
